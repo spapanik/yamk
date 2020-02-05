70af52d1d0c3271bf06b993da95857e01ec1503a
Stephanos Kuma <stephanos.papanikolopoulos@oaknorth.ai>
2020-02-05 18:17:20 +0000

    Add more tests

--- tests/yamk/test_make.py
+++ tests/yamk/test_make.py
@@ -1,3 +1,4 @@
+import os
 from unittest import mock
 
 import pytest
@@ -38,6 +39,38 @@ class TestMakeCommand:
         assert runner.call_count == 2
 
     @staticmethod
+    @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=42))
+    def test_make_builds_wrong_command_breaks(runner):
+        args = mock.MagicMock()
+        args.target = "failure"
+        args.makefile = TESTS_ROOT.joinpath("data", "wrong_commands.toml")
+        make_command = make.MakeCommand(args)
+        with pytest.raises(SystemExit) as exc:
+            make_command.make()
+        assert exc.value.code == 42
+        assert runner.call_count == 1
+
+    @staticmethod
+    @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=42))
+    def test_make_builds_wrong_command_with_allowed_failure(runner):
+        args = mock.MagicMock()
+        args.target = "allowed_failure"
+        args.makefile = TESTS_ROOT.joinpath("data", "wrong_commands.toml")
+        make_command = make.MakeCommand(args)
+        make_command.make()
+        assert runner.call_count == 2
+
+    @staticmethod
+    @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=42))
+    def test_make_builds_wrong_command_with_allowed_failure_in_command(runner):
+        args = mock.MagicMock()
+        args.target = "allowed_failure_in_command"
+        args.makefile = TESTS_ROOT.joinpath("data", "wrong_commands.toml")
+        make_command = make.MakeCommand(args)
+        make_command.make()
+        assert runner.call_count == 1
+
+    @staticmethod
     @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=0))
     def test_make_extra_vars(runner):
         args = mock.MagicMock()
@@ -46,3 +79,39 @@ class TestMakeCommand:
         make_command = make.MakeCommand(args)
         make_command.make()
         runner.assert_called_once_with("echo this/phony.txt", shell=True)
+
+    @staticmethod
+    @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=0))
+    def test_make_strong_var(runner):
+        os.environ["FOO"] = "FOO"
+        os.environ["BAR"] = "BAR"
+        args = mock.MagicMock()
+        args.target = "strong_var"
+        args.makefile = TESTS_ROOT.joinpath("data", "extra_vars.toml")
+        make_command = make.MakeCommand(args)
+        make_command.make()
+        runner.assert_called_once_with("echo foo BAR", shell=True)
+
+    @staticmethod
+    @mock.patch("yamk.make.print", new_callable=mock.MagicMock)
+    @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=0))
+    def test_make_echo_in_recipe(runner, mock_print):
+        args = mock.MagicMock()
+        args.target = "echo"
+        args.makefile = TESTS_ROOT.joinpath("data", "echo_command.toml")
+        make_command = make.MakeCommand(args)
+        make_command.make()
+        mock_print.assert_called_once_with("ls")
+        assert runner.call_count == 1
+
+    @staticmethod
+    @mock.patch("yamk.make.print", new_callable=mock.MagicMock)
+    @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=0))
+    def test_make_echo_in_command(runner, mock_print):
+        args = mock.MagicMock()
+        args.target = "echo_in_command"
+        args.makefile = TESTS_ROOT.joinpath("data", "echo_command.toml")
+        make_command = make.MakeCommand(args)
+        make_command.make()
+        mock_print.assert_called_once_with("ls")
+        assert runner.call_count == 1
