5eb9a7852c3231aeb6e51d7a3e195d7594f6b1fe
Stephanos Kuma <stephanos.papanikolopoulos@oaknorth.ai>
2020-02-18 16:56:04 +0000

    Add a single toml for tests

--- tests/settings.py
+++ tests/settings.py
@@ -1,3 +1,4 @@
 import pathlib
 
 TESTS_ROOT = pathlib.Path(__file__).absolute().parent
+TEST_MAKEFILE = TESTS_ROOT.joinpath("data", "mk.toml")
--- tests/yamk/test_make.py
+++ tests/yamk/test_make.py
@@ -5,7 +5,7 @@ import pytest
 
 from yamk import make
 
-from tests.settings import TESTS_ROOT
+from tests.settings import TEST_MAKEFILE
 
 
 class TestMakeCommand:
@@ -13,7 +13,8 @@ class TestMakeCommand:
     @mock.patch("yamk.make.open", new_callable=mock.mock_open, read_data="")
     def test_make_raises_on_missing_target(_mock_obj):
         args = mock.MagicMock()
-        args.target = "phony"
+        args.target = "missing_target"
+        args.makefile = TEST_MAKEFILE
         make_command = make.MakeCommand(args)
         with pytest.raises(ValueError):
             make_command.make()
@@ -23,7 +24,7 @@ class TestMakeCommand:
     def test_make_builds_with_no_commands(runner):
         args = mock.MagicMock()
         args.target = "phony"
-        args.makefile = TESTS_ROOT.joinpath("data", "empty_phony.toml")
+        args.makefile = TEST_MAKEFILE
         make_command = make.MakeCommand(args)
         make_command.make()
         runner.assert_not_called()
@@ -32,8 +33,8 @@ class TestMakeCommand:
     @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=0))
     def test_make_builds_with_two_commands(runner):
         args = mock.MagicMock()
-        args.target = "phony"
-        args.makefile = TESTS_ROOT.joinpath("data", "two_commands.toml")
+        args.target = "two_commands"
+        args.makefile = TEST_MAKEFILE
         make_command = make.MakeCommand(args)
         make_command.make()
         assert runner.call_count == 2
@@ -43,7 +44,7 @@ class TestMakeCommand:
     def test_make_builds_wrong_command_breaks(runner):
         args = mock.MagicMock()
         args.target = "failure"
-        args.makefile = TESTS_ROOT.joinpath("data", "wrong_commands.toml")
+        args.makefile = TEST_MAKEFILE
         make_command = make.MakeCommand(args)
         with pytest.raises(SystemExit) as exc:
             make_command.make()
@@ -55,7 +56,7 @@ class TestMakeCommand:
     def test_make_builds_wrong_command_with_allowed_failure(runner):
         args = mock.MagicMock()
         args.target = "allowed_failure"
-        args.makefile = TESTS_ROOT.joinpath("data", "wrong_commands.toml")
+        args.makefile = TEST_MAKEFILE
         make_command = make.MakeCommand(args)
         make_command.make()
         assert runner.call_count == 2
@@ -65,7 +66,7 @@ class TestMakeCommand:
     def test_make_builds_wrong_command_with_allowed_failure_in_command(runner):
         args = mock.MagicMock()
         args.target = "allowed_failure_in_command"
-        args.makefile = TESTS_ROOT.joinpath("data", "wrong_commands.toml")
+        args.makefile = TEST_MAKEFILE
         make_command = make.MakeCommand(args)
         make_command.make()
         assert runner.call_count == 1
@@ -73,24 +74,16 @@ class TestMakeCommand:
     @staticmethod
     @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=0))
     def test_make_extra_vars(runner):
+        os.environ["filename"] = "service.conf"
+        os.environ["conf_dir"] = "service.d"
         args = mock.MagicMock()
-        args.target = "phony"
-        args.makefile = TESTS_ROOT.joinpath("data", "extra_vars.toml")
+        args.target = "variables"
+        args.makefile = TEST_MAKEFILE
         make_command = make.MakeCommand(args)
         make_command.make()
-        runner.assert_called_once_with("echo this/phony.txt", shell=True)
-
-    @staticmethod
-    @mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=0))
-    def test_make_strong_var(runner):
-        os.environ["FOO"] = "FOO"
-        os.environ["BAR"] = "BAR"
-        args = mock.MagicMock()
-        args.target = "strong_var"
-        args.makefile = TESTS_ROOT.joinpath("data", "extra_vars.toml")
-        make_command = make.MakeCommand(args)
-        make_command.make()
-        runner.assert_called_once_with("echo foo BAR", shell=True)
+        runner.assert_called_once_with(
+            "echo /path/to/local/conf/local.service.d/service.conf", shell=True
+        )
 
     @staticmethod
     @mock.patch("yamk.make.print", new_callable=mock.MagicMock)
@@ -98,7 +91,7 @@ class TestMakeCommand:
     def test_make_echo_in_recipe(runner, mock_print):
         args = mock.MagicMock()
         args.target = "echo"
-        args.makefile = TESTS_ROOT.joinpath("data", "echo_command.toml")
+        args.makefile = TEST_MAKEFILE
         make_command = make.MakeCommand(args)
         make_command.make()
         mock_print.assert_called_once_with("ls")
@@ -110,7 +103,7 @@ class TestMakeCommand:
     def test_make_echo_in_command(runner, mock_print):
         args = mock.MagicMock()
         args.target = "echo_in_command"
-        args.makefile = TESTS_ROOT.joinpath("data", "echo_command.toml")
+        args.makefile = TEST_MAKEFILE
         make_command = make.MakeCommand(args)
         make_command.make()
         mock_print.assert_called_once_with("ls")
