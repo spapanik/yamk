a4c2d9a77933d227aaaccffc9690093281ed4006
Stephanos Kuma <stephanos@kuma.ai>
2021-12-16 19:51:29 +0000

    Change doc theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,5 +8,5 @@ extensions: List[str] = []
 templates_path = ["_templates"]
 exclude_patterns: List[str] = []
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
