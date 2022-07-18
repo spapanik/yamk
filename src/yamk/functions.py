386d2160f8c623dcec6af3997b7d01757ff0cb50
Stephanos Kuma <stephanos@kuma.ai>
2022-07-18 18:54:09 +0100

    Remove unnecessary metaclass

--- src/yamk/functions.py
+++ src/yamk/functions.py
@@ -1,19 +1,10 @@
 import pathlib
 from functools import reduce
-from typing import Dict
 
 
-class FunctionMeta(type):
-    functions: Dict[str, "Function"] = {}
+class Function:
+    name: str
 
-    def __new__(mcs, name, bases, class_dict):
-        x = super().__new__(mcs, name, bases, class_dict)
-        if getattr(x, "name", None) is not None:
-            mcs.functions[x.name] = x
-        return x
-
-
-class Function(metaclass=FunctionMeta):
     def __init__(self, base_dir: pathlib.Path):
         self.base_dir = base_dir
 
@@ -137,4 +128,4 @@ class Merge(Function):
         return reduce(lambda x, y: self._as_list(x) + self._as_list(y), args)
 
 
-functions = FunctionMeta.functions
+functions = {function.name: function for function in Function.__subclasses__()}
