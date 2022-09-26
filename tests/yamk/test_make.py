f556a6145ce85925b6471624ecf7eb1110702526
Stephanos Kuma <stephanos@kuma.ai>
2022-09-26 21:44:59 +0100

    Add more test cases

--- tests/yamk/test_make.py
+++ tests/yamk/test_make.py
@@ -458,3 +458,19 @@ def test_make_with_d_override(runner, mock_args):
     assert runner.call_count == 1
     calls = [mock.call("echo tail", **make_command.subprocess_kwargs)]
     assert sorted(runner.call_args_list) == sorted(calls)
+
+
+@mock.patch("yamk.make.subprocess.run", return_value=mock.MagicMock(returncode=0))
+def test_make_with_d_override_vars(runner, mock_args):
+    mock_args.target = ".d_override_vars"
+    make_command = make.MakeCommand(mock_args)
+    make_command.make()
+    assert runner.call_count == 3
+    calls = [
+        mock.call("echo 3 4", **make_command.subprocess_kwargs),
+        mock.call(
+            "echo {'x': 'a', 'y': '2', 'z': 'final'}", **make_command.subprocess_kwargs
+        ),
+        mock.call("echo 3", **make_command.subprocess_kwargs),
+    ]
+    assert runner.call_args_list == calls
