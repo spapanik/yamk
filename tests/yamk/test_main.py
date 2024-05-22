d64130f5e82bf63a943b937b785b26585e36250f
Stephanos Kuma <stephanos@kuma.ai>
2024-05-22 16:38:20 +0100

    ♻️ Rename main module

--- tests/yamk/test_main.py
+++ tests/yamk/test_main.py
@@ -1,17 +1,17 @@
 from unittest import mock
 
-from yamk import main
+from yamk import __main__
 
 
 @mock.patch("sys.argv", ["yamk", "phony"])
 def test_parse_args() -> None:
-    args = main.parse_args()
+    args = __main__.parse_args()
     assert args.target == "phony"
 
 
 @mock.patch("sys.argv", ["yamk", "phony"])
-@mock.patch("yamk.main.MakeCommand")
+@mock.patch("yamk.__main__.MakeCommand")
 def test_main(mock_make: mock.MagicMock) -> None:
-    main.main()
+    __main__.main()
     assert mock_make.call_count == 1
     assert mock_make().make.call_count == 1
