5b6453a570be51b9b7b7b1fcaef1c6496f735e03
Stephanos Kuma <stephanos@kuma.ai>
2024-11-18 15:34:09 +0000

    🔬 Cleanup remaining tests

--- tests/yamk/command/test_make/test_make.py
+++ tests/yamk/command/test_make/test_make.py
@@ -1,62 +1,49 @@
 from unittest import mock
 
-from yamk.command import make
+from tests.conftest import get_make_command, runner_exit_success
+
+COOKBOOK = "make.yaml"
 
 
-@mock.patch("yamk.command.make.subprocess.run")
-def test_make_builds_with_no_commands(
-    runner: mock.MagicMock, mock_args: mock.MagicMock
-) -> None:
-    mock_args.target = "phony"
-    make_command = make.MakeCommand(mock_args)
+@mock.patch("yamk.command.make.subprocess.run", new_callable=runner_exit_success)
+def test_make_builds_with_no_commands(runner: mock.MagicMock) -> None:
+    make_command = get_make_command(cookbook_name=COOKBOOK, target="no_commands")
     make_command.make()
     assert runner.call_count == 0
 
 
-@mock.patch(
-    "yamk.command.make.subprocess.run", return_value=mock.MagicMock(returncode=0)
-)
-def test_make_builds_with_two_commands(
-    runner: mock.MagicMock, mock_args: mock.MagicMock
-) -> None:
-    mock_args.target = "two_commands"
-    make_command = make.MakeCommand(mock_args)
+@mock.patch("yamk.command.make.subprocess.run", new_callable=runner_exit_success)
+def test_make_builds_with_two_commands(runner: mock.MagicMock) -> None:
+    make_command = get_make_command(cookbook_name=COOKBOOK, target="two_commands")
     make_command.make()
     assert runner.call_count == 2
     calls = [
-        mock.call("ls", **make_command.subprocess_kwargs),
+        mock.call("echo two_commands", **make_command.subprocess_kwargs),
         mock.call("echo 42", **make_command.subprocess_kwargs),
     ]
     assert runner.call_args_list == calls
 
 
-@mock.patch(
-    "yamk.command.make.subprocess.run", return_value=mock.MagicMock(returncode=0)
-)
-def test_make_alias(runner: mock.MagicMock, mock_args: mock.MagicMock) -> None:
-    mock_args.target = "alias"
-    make_command = make.MakeCommand(mock_args)
+@mock.patch("yamk.command.make.subprocess.run", new_callable=runner_exit_success)
+def test_make_alias(runner: mock.MagicMock) -> None:
+    make_command = get_make_command(cookbook_name=COOKBOOK, target="alias")
     make_command.make()
     assert runner.call_count == 2
     calls = [
-        mock.call("ls", **make_command.subprocess_kwargs),
+        mock.call("echo two_commands", **make_command.subprocess_kwargs),
         mock.call("echo 42", **make_command.subprocess_kwargs),
     ]
     assert runner.call_args_list == calls
 
 
-@mock.patch(
-    "yamk.command.make.subprocess.run", return_value=mock.MagicMock(returncode=0)
-)
-def test_make_with_requirements(
-    runner: mock.MagicMock, mock_args: mock.MagicMock
-) -> None:
-    mock_args.target = "requires"
-    make_command = make.MakeCommand(mock_args)
+@mock.patch("yamk.command.make.subprocess.run", new_callable=runner_exit_success)
+def test_make_with_requirements(runner: mock.MagicMock) -> None:
+    make_command = get_make_command(cookbook_name=COOKBOOK, target="with_requirements")
     make_command.make()
-    assert runner.call_count == 2
+    assert runner.call_count == 3
     calls = [
-        mock.call("ls", **make_command.subprocess_kwargs),
-        mock.call("true", **make_command.subprocess_kwargs),
+        mock.call("echo two_commands", **make_command.subprocess_kwargs),
+        mock.call("echo 42", **make_command.subprocess_kwargs),
+        mock.call("echo with_requirements", **make_command.subprocess_kwargs),
     ]
     assert runner.call_args_list == calls
